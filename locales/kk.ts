52437c76ab66934842e3fb7857377634ee6948c0
Vladimir Mysko <57433511+vladimirmysko@users.noreply.github.com>
2024-03-19 16:14:19 +0500

    feat: configure project

---
+++ locales/kk.ts
@@ -0,0 +1,5 @@
+export default {
+  title: 'Vladimir Mysko — Design Engineer',
+  vladimir_mysko: 'Vladimir Mysko',
+  design_engineer: 'Design engineer.',
+} as const;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      white: '#fff',
+      black: '#000',
+    },
+    fontFamily: {
+      sans: ['var(--font-inter-display)'],
+    },
+    fontSize: {
+      base: ['1.0625rem', '1.375rem'],
+    },
+    fontWeight: {
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
     },
   },
-  plugins: [],
+  plugins: [typography, forms],
 };
+
 export default config;
