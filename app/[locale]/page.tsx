f858d3a70842bb2f3826a0af0ba62ad61b9ab6c8
Vladimir Mysko <57433511+vladimirmysko@users.noreply.github.com>
2024-05-07 18:59:53 +0500

    Update home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -10,8 +10,34 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
+interface Description {
+  term: string;
+  data: string | React.ReactElement;
+}
+
 export default async function HomePage() {
   const t = await getI18n();
 
-  return <main className="flex min-h-svh flex-col items-stretch"></main>;
+  const descriptionList: Description[] = [
+    { term: 'MYSKO', data: t('home.web_development_design') },
+    { term: t('home.information'), data: t('home.web_development_design') },
+    { term: t('home.contacts'), data: t('home.web_development_design') },
+    { term: t('home.works'), data: t('home.web_development_design') },
+    { term: t('home.technologies'), data: t('home.web_development_design') },
+  ];
+
+  return (
+    <main className="flex min-h-svh flex-col items-stretch">
+      <div className="mx-auto flex w-full max-w-3xl flex-col items-stretch gap-10 p-5">
+        <dl className="flex flex-col items-stretch gap-10">
+          {descriptionList.map((description) => (
+            <div key={description.term} className="flex flex-row gap-5">
+              <dt className="flex-1 text-right font-semibold tracking-tight">{description.term}</dt>
+              <dd className="flex-1">{description.data}</dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </main>
+  );
 }
--- locales/en.ts
+++ locales/en.ts
@@ -2,5 +2,10 @@ export default {
   home: {
     set_title: 'Mysko',
     seo_description: '',
+    web_development_design: 'Web Development, Design, Uralsk, Kazakhstan, 2024',
+    information: 'INFORMATION',
+    contacts: 'CONTACTS',
+    works: 'WORKS',
+    technologies: 'TECHNOLOGIES',
   },
 } as const;
