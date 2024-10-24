d9cd23c1bc919aa89cbdce9407b301ec63b9d600
Vladimir Mysko <57433511+vladimirmysko@users.noreply.github.com>
2024-10-24 17:03:47 +0500

    feat: Create an empty Next.js project

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss';
+
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [forms, typography],
+};
+
+export default config;
