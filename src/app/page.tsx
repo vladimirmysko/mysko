aa14c0e53157e07af3c5cf2e4170b1c311787048
Vladimir Mysko <57433511+vladimirmysko@users.noreply.github.com>
2026-02-15 20:36:19 +0500

    Revise homepage content to reflect personal information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,50 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="flex h-screen flex-col items-center overflow-x-hidden overflow-y-auto px-4">
+      <main className="grid w-full max-w-3xl grid-cols-1 gap-12 py-20">
+        <h1 className="text-3xl font-semibold">Владимир Мысько.</h1>
+
+        <div className="grid grid-cols-1 gap-4">
+          <p>Работаю fullstack-разработчиком и преподаю в университете.</p>
+          <p>
+            Делаю интерфейсы на React / Next.js и стараюсь, чтобы они были
+            понятными, быстрыми и доступными.
+          </p>
+          <p>
+            Когда нужно - подключаюсь к серверной части: Next.js (RSC), Node.js
+            / Bun, Python, базы Postgres / SQLite, ORM Prisma.
+          </p>
+          <p>
+            Предпочитаю спокойные решения: меньше сложностей, больше
+            предсказуемости. Этот сайт собран на Next.js со статической сборкой
+            и минимумом клиентской логики.
+          </p>
+          <p>Сейчас ищу вторую работу или подработку (part-time / проектно).</p>
+          <p>
+            Вы можете найти меня в{" "}
+            <Link href="https://x.com/vladimirmysko">X</Link>,{" "}
+            <Link href="https://github.com/vladimirmysko">GitHub</Link> и{" "}
+            <Link href="https://t.me/vladimirmysko">Telegram</Link>. Также вы
+            можете написать мне по электронной почте:{" "}
+            <Link href="mailto:vladimir.mysko@gmail.com">
+              vladimir.mysko@gmail.com
+            </Link>
+            .
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 transition-colors hover:bg-[#383838] md:w-[158px] dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="grid grid-cols-1 gap-4">
+          <h2 className="text-2xl font-semibold">Проекты</h2>
+          <ul className="list-inside list-disc"></ul>
         </div>
       </main>
+
+      <footer className="flex w-full max-w-3xl flex-row items-center justify-between py-12">
+        <p>Copyright © mysko</p>
+        <Link href="https://github.com/vladimirmysko/mysko">Исходный код</Link>
+      </footer>
     </div>
   );
 }
